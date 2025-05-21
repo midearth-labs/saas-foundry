05732642056dd9d160203349590b35f29c8e8bd4
Awwal M. <67423428+awwalm@users.noreply.github.com>
2025-05-21 17:50:59 +0100

    Reset org-authZ procedure; updated MDC glob paths

--- packages/api/src/trpc/base-procedures/organization-authorized.ts
+++ packages/api/src/trpc/base-procedures/organization-authorized.ts
@@ -1,3 +1,4 @@
+import { authenticatedProcedure } from "./authenticated";
 import { inferProcedureBuilderResolverOptions, TRPCError } from "@trpc/server";
 import { authorizedProcedure } from "./authorized-procedure";
 
@@ -5,16 +6,22 @@ import { authorizedProcedure } from "./authorized-procedure";
 // TODO this is to be inspected and reimplemented as organizational permissions are dynamic
 // also check implementation of checkOrgPermission
 export const organizationAuthorizedProcedure = authorizedProcedure.use(async ({ ctx, next, meta }) => {
-    // const session = await ctx.auth.checkValidSession();
+    /*const session = await ctx.in.getSessionOrThrow();
     const permission = meta?.permission;
+    
+    if (!session.session.activeOrganizationId) {
+        throw new TRPCError({ 
+            code: 'FORBIDDEN', 
+            message: 'This operation requires an active organization context' 
+        });
+    }
 
     if (permission) {
-        let perm = await ctx.auth.checkOrgAuthorized(permission);  // Only check org-level permission
-        console.log("Permission check result:", JSON.stringify(perm, null, 2));
+        await ctx.in.checkOrgAuthorized(session, permission);  // Only check org-level permission
     } else {
         console.error("No permissions defined in meta for operation");
         throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: 'Internal server error' });
-    }
+    }*/
     
     return next({ ctx: { ...ctx } });
 });
